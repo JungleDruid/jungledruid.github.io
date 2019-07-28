9786e89844dd08a0965931269b53f74f4f833182
Yonsh Lin <yonsh@live.com>
2019-07-28 21:03:15 +0800

    Update

---
+++ precache-manifest.6a2f5cb993bb1a2eb645c890d85595da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed091713a644863ca72d8885c60a1fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55ac56afcbb50397d4e0",
+    "url": "/static/css/main.c060bb6d.chunk.css"
+  },
+  {
+    "revision": "e962c03a78c09c33e6b0",
+    "url": "/static/js/2.7757beec.chunk.js"
+  },
+  {
+    "revision": "55ac56afcbb50397d4e0",
+    "url": "/static/js/main.59506de5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
